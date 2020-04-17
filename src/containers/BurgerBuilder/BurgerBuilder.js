dba87176f601e15dd532bc274c6e9c0bdb7e5413
Rajiv Khushiram <31241377+Rajiv-Khushiram@users.noreply.github.com>
2020-04-18 03:34:03 +1000

    changed hoc/aux to auxillary as its a reserved keyword

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/auxillary'
 import Burger from '../../components/Burger/Burger'
 class BurgerBuilder extends Component {
     // constructor(props) {
