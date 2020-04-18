3f22cbf3e064716f1b97d939eb6fb2202dc3042b
Rajiv Khushiram <31241377+Rajiv-Khushiram@users.noreply.github.com>
2020-04-18 18:22:41 +1000

    adding the order button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,23 +1,32 @@
 import React from "react";
-import BuildControl from './BuildControl/BuildControl'
+import BuildControl from "./BuildControl/BuildControl";
 import classes from "./BuildControls.module.css";
 
-
 const controls = [
-    {label:'Salad', type:'salad'},
-    {label:'Bacon', type:'bacon'},
-    {label:'Cheese', type:'cheese'},
-    {label:'Meat', type:'meat'}
-]
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
 
 const BuildControls = (props) => (
-<div className={classes.BuildControls}>
-   <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p> 
-    {controls.map(ctrl => (
-        <BuildControl  key={ctrl.label} label={ctrl.label} added={()=>props.ingredientAdded(ctrl.type)}
-        removed={()=>props.ingredientRemoved(ctrl.type)} disable={props.disabled[ctrl.type]}  />
-    )
-    )}
-</div>);
+  <div className={classes.BuildControls}>
+    <p>
+      Current Price: <strong>{props.price.toFixed(2)}</strong>
+    </p>
+    {controls.map((ctrl) => (
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disable={props.disabled[ctrl.type]}
+      />
+    ))}
+    <button disabled={!props.purchasable} className={classes.OrderButton}>
+      ORDER NOW
+    </button>
+  </div>
+);
 
 export default BuildControls;
