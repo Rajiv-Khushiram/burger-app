b94b5d0ddd6a34ea3b113b26213a64fdc20a3a2d
Rajiv Khushiram <31241377+Rajiv-Khushiram@users.noreply.github.com>
2020-04-18 20:34:31 +1000

    adding more responsive content to sidedrawer and toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,7 @@ const Toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <div className={classes.Logo}><Logo/></div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
     </header>
